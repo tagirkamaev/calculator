d9f384d7df057ef6ed6cdc4225788fc04d5c4fe8
tagirkamaev <180089636+tagirkamaev@users.noreply.github.com>
2024-11-18 13:41:28 +0300

    Created files
    Created files and added function for operations

---
+++ myscripts.js
@@ -0,0 +1,20 @@
+// 1. Create functions for operations
+// write a function 'add'
+function add(a, b) {
+  return a + b;
+}
+
+// write a function 'subtract'
+function subtract(a, b) {
+  return a - b;
+}
+
+// write a function 'multiply'
+function multiply(a, b) {
+  return a * b;
+}
+
+// write a function 'divide'
+function divide(a, b) {
+  return a / b;
+}
